bbe67f64bdb5ff23503d4c2d389c6dd88a87f220
Stanislav_Kaygorodov <stasmen@mail.ru>
2024-04-30 14:57:12 +0300

    Homework02 finish

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -6,7 +6,7 @@ namespace Homework02
     {
         public static void Main(string[] args)
         {
-            const int fieldNameSize = 10;
+            const int fieldNameSize = 32;
 
             string fullNaneFieldName = "Ф.И.";
             string ageFieldName = "Возраст";
@@ -14,24 +14,43 @@ namespace Homework02
             string programmingScoreFieldName = "Баллы по программированию";
             string mathScoreFieldName = "Баллы по математике";
             string physicsScoreFieldName = "Баллы по физике";
+            string avgProgrammingScoreFieldName = "Средний бал по программированию";
+            string avgMathScoreFieldName = "Средний бал по математике";
+            string avgPhysicsScoreFieldName = "Средний бал по физике";
             
             string firstName = "Иван";
             string lastName = "Пупкин";
             string fullName = firstName + lastName;
             int age = 15;
             string email = "pupkin@mail.ru";
-            float programmingScore = 4.5f;
-            float mathScore = 4f;
-            float physicsScore = 5f;
+            float programmingScore1 = 4, programmingScore2 = 5;
+            float mathScore1 = 3, mathScore2 = 5;
+            float physicsScore1 = 5, physicsScore2 = 4;
             
-            string data = $"{fullNaneFieldName, fieldNameSize}: {fullName}\n";
-            data += $"{ageFieldName, fieldNameSize}: {age}\n";
-            data += $"{emailFieldName, fieldNameSize}: {email}\n";
-            data += $"{programmingScoreFieldName, fieldNameSize}: {programmingScore}\n";
-            data += $"{mathScoreFieldName, fieldNameSize}: {mathScore}\n";
-            data += $"{physicsScoreFieldName, fieldNameSize}: {physicsScore}\n";
+            float programmingScoreSum = programmingScore1 + programmingScore2;
+            float mathScoreSum = mathScore1 + mathScore2;
+            float physicsScoreSum = physicsScore1 + physicsScore2;
+
+            float avgProgrammingScore = programmingScoreSum / 2;
+            float avgMathScore = mathScoreSum / 2;
+            float avgPhysicsScore = physicsScoreSum / 2;
             
-            Console.WriteLine(data);
+            string mainData = $"{fullNaneFieldName, fieldNameSize}: {fullName}\n";
+            mainData += $"{ageFieldName, fieldNameSize}: {age}\n";
+            mainData += $"{emailFieldName, fieldNameSize}: {email}\n";
+            mainData += $"{programmingScoreFieldName, fieldNameSize}: {programmingScoreSum}\n";
+            mainData += $"{mathScoreFieldName, fieldNameSize}: {mathScoreSum}\n";
+            mainData += $"{physicsScoreFieldName, fieldNameSize}: {physicsScoreSum}\n";
+
+            string avgData = $"{avgProgrammingScoreFieldName, fieldNameSize}: {avgProgrammingScore}\n";
+            avgData += $"{avgMathScoreFieldName, fieldNameSize}: {avgMathScore}\n";
+            avgData += $"{avgPhysicsScoreFieldName, fieldNameSize}: {avgPhysicsScore}\n";
+            
+            Console.WriteLine(mainData);
+            Console.ReadKey();
+            
+            Console.WriteLine(avgData);
+            Console.ReadKey();
         }
     }
 }
