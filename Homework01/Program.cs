85795a597a26136279f2341b1eb44fc0ed75bfe8
Stanislav_Kaygorodov <stasmen@mail.ru>
2024-04-30 12:35:18 +0300

    homework02

---
+++ Homework01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("world");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int fieldNameSize = 10;
+
+            string fullNaneFieldName = "Ф.И.";
+            string ageFieldName = "Возраст";
+            string emailFieldName = "Email";
+            string programmingScoreFieldName = "Баллы по программированию";
+            string mathScoreFieldName = "Баллы по математике";
+            string physicsScoreFieldName = "Баллы по физике";
+            
+            string firstName = "Иван";
+            string lastName = "Пупкин";
+            string fullName = firstName + lastName;
+            int age = 15;
+            string email = "pupkin@mail.ru";
+            float programmingScore = 4.5f;
+            float mathScore = 4f;
+            float physicsScore = 5f;
+            
+            string data = $"{fullNaneFieldName, fieldNameSize}: {fullName}\n";
+            data += $"{ageFieldName, fieldNameSize}: {age}\n";
+            data += $"{emailFieldName, fieldNameSize}: {email}\n";
+            data += $"{programmingScoreFieldName, fieldNameSize}: {programmingScore}\n";
+            data += $"{mathScoreFieldName, fieldNameSize}: {mathScore}\n";
+            data += $"{physicsScoreFieldName, fieldNameSize}: {physicsScore}\n";
+            
+            Console.WriteLine(data);
+        }
+    }
+}
