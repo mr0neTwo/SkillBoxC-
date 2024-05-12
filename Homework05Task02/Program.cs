e847fab33ba979c86383758ca2e78fc2ebe21d3b
stas <stasmen@mail.ru>
2024-05-12 16:26:49 +0300

    Homework 5 task 2

---
+++ Homework05Task02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Homework05Task02
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter any sentence: ");
+			string sentence = Console.ReadLine();
+			string reversedSentence = ReversWords(sentence);
+
+			Console.WriteLine(sentence);
+			Console.WriteLine(reversedSentence);
+		}
+		
+		private static string ReversWords(string sentence)
+		{
+			string[] words = SplitSentence(sentence);
+			string[] reversedWords = words.Reverse().ToArray();
+			string reversedSentence = string.Join(" ", reversedWords);
+
+			return reversedSentence;
+		}
+		
+		private static string[] SplitSentence(string sentence)
+		{
+			string[] words = sentence.Split(' ');
+
+			return words;
+		}
+	}
+}
