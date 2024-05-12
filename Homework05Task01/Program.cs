eb6ef92c64d1bdd95c7df6dab9cf013219773b4e
stas <stasmen@mail.ru>
2024-05-12 16:14:03 +0300

    Homework 5 task 1

---
+++ Homework05Task01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework05Task01
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter any sentence: ");
+			string sentence = Console.ReadLine();
+			string[] words = SplitSentence(sentence);
+			PrintWords(words);
+		}
+
+		private static string[] SplitSentence(string sentence)
+		{
+			string symbolsToRemove = ".,;:)({}!?";
+
+			foreach (char symbol in symbolsToRemove)
+			{
+				sentence = sentence.Replace(symbol.ToString(), "");
+			}
+			
+			string[] words = sentence.Split(' ');
+
+			return words;
+		}
+
+		private static void PrintWords(string[] words)
+		{
+			foreach (string word in words)
+			{
+				Console.WriteLine(word);
+			}
+		}
+	}
+}
