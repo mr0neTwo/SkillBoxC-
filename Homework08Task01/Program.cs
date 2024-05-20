bf81eaec61a079c63e253b8ef40a6a19209a4592
stas <stasmen@mail.ru>
2024-05-20 18:15:46 +0300

    homework 08 task 01

---
+++ Homework08Task01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework08Task01
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<int> numbers = GetRandomIntegerList(100, 0, 100);
+			PrintIntegerList(numbers);
+			RemoveAllValueInRange(numbers, 25, 50);
+			PrintIntegerList(numbers);
+		}
+
+		private static List<int> GetRandomIntegerList(int count, int startValue, int endValue)
+		{
+			Random random = new Random();
+			List<int> numbers = new List<int>();
+
+			for (int i = 0; i < count; i++)
+			{
+				numbers.Add(random.Next(startValue, endValue));
+			}
+
+			return numbers;
+		}
+
+		private static void PrintIntegerList(List<int> list)
+		{
+			Console.WriteLine(string.Join(" ", list));
+		}
+
+		private static void RemoveAllValueInRange(List<int> list, int startRange, int endRange)
+		{
+			list.RemoveAll(number => number > startRange && number < endRange);
+		}
+	}
+}
