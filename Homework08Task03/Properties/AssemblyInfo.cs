e7e848099cb0131e5a21581ed835dd6060467a53
stas <stasmen@mail.ru>
2024-05-20 22:56:54 +0300

    rename homework08task03

--- Homework08Task03/Properties/AssemblyInfo.cs
+++ Homework08Task03/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly : AssemblyTitle("Homework01Task03")]
+[assembly : AssemblyTitle("Homework08Task03")]
 [assembly : AssemblyDescription("")]
 [assembly : AssemblyConfiguration("")]
 [assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("Homework01Task03")]
+[assembly : AssemblyProduct("Homework08Task03")]
 [assembly : AssemblyCopyright("Copyright ©  2024")]
 [assembly : AssemblyTrademark("")]
 [assembly : AssemblyCulture("")]
